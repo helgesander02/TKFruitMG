dfee3ba254823573b679813a3e8668a32bd19fa9
Lazyboy0204 <luckyykyo960119@gmail.com>
2023-06-04 22:39:08 +0800

    0604

--- Accounting/accounting.py
+++ Accounting/accounting.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import os
 import tkcalendar as tkc
+import psycopg2
 
 from typing import Optional, Tuple, Union
 from PIL import ImageTk,Image
@@ -12,8 +13,8 @@ class right_top_part(ctk.CTkFrame):
         self.address = ctk.CTkLabel(self,text="地址：",font=("Arial",20),fg_color="pink")
         self.phone = ctk.CTkLabel(self,text="　　手機：",font=("Arial",20),fg_color="green")
         self.remark = ctk.CTkLabel(self,text="備註：",font=("Arial",20),fg_color="brown")
-        self.name_entry = ctk.CTkEntry(self)
-        self.phone_entry = ctk.CTkEntry(self)
+        self.name_entry = ctk.CTkLabel(self)
+        self.phone_entry = ctk.CTkLabel(self)
         self.address_entry = ctk.CTkTextbox(self,height=90)
         self.remark_entry = ctk.CTkTextbox(self,height=90)
 
@@ -25,18 +26,61 @@ class right_top_part(ctk.CTkFrame):
         self.address_entry.place(x=450,y=50)
         self.remark.place(x=680,y=50)
         self.remark_entry.place(x=750,y=50)
-        
+
+class item(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.chk_box = ctk.CTkCheckBox(self,width=80,height=40,text="")
+        self.chk_box.grid(row=0,column=0,sticky="news")
+        self.dat = ctk.CTkLabel(self,width=176,height=40,text=f"銷貨日期")
+        self.dat.grid(row=0,column=1,sticky="news")
+        self.sbtotal = ctk.CTkLabel(self,width=176,height=40,text=f"銷貨總計")
+        self.sbtotal.grid(row=0,column=2,sticky="news")
+        self.al_total = ctk.CTkEntry(self,width=176,height=40)
+        self.al_total.grid(row=0,column=3,sticky="news")
+        self.overage = ctk.CTkLabel(self,width=176,height=40,text="")
+        self.overage.grid(row=0,column=4,sticky="news")
+        self.remark = ctk.CTkLabel(self,width=176,height=40,text=f"備註")
+        self.remark.grid(row=0,column=5,sticky="news")
 class left_part(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         def search():
-            pass
+            con = psycopg2.connect(database='postgres', user='postgres',
+                       password='admin')
+            cur = con.cursor()
+            cur.execute(f"select name, phone, address, remark from customer where c_id='{customer_id_entry.get()}'")
+            result = cur.fetchone()
+            cur.execute(f"select * from order_it where c_id='{customer_id_entry.get()}' and date='{sell_date_entry.get_date()}'")
+            result2 = cur.fetchall()
+            right_top.name_entry.configure(text=f"{str(result[0]).rstrip()}")
+            right_top.phone_entry.configure(text=f"{str(result[1]).rstrip()}")
+            right_top.address_entry.insert(1.0,f"{str(result[2]).rstrip()}")
+            right_top.remark_entry.insert(1.0,f"{str(result[3]).rstrip()}")
+            cur.close()
+            con.close()
+            row = 0
+            s_total = 0
+            for i in result2:
+                s_total += int(result2[row][6])
+                it = item(right_bot.mid,fg_color="#EEEEEE")
+                it.dat.configure(text=f"{result2[row][9]}")
+                it.sbtotal.configure(text=f"{result2[row][6]}")
+                it.remark.configure(text=f"{result2[row][7]}")
+                it.pack()
+                row += 1
+            right_bot.label.configure(text="總計："+str(s_total))
         def reset():
             customer_id_entry.delete(0,'end')
             sell_date_entry.delete(0,'end')
+            right_top.name_entry.configure(text="")
+            right_top.phone_entry.configure(text="")
+            right_top.address_entry.delete(1.0,'end')
+            right_top.remark_entry.delete(1.0,'end')
+            right_bot.label.configure(text="總計：")
         customer_id_entry = ctk.CTkEntry(self,width=200, height=40,fg_color="yellow",font=("Arial", 20),placeholder_text="客戶代號")
         sell_date_entry = tkc.DateEntry(self,selectmode='day')
-        confirm_btn = ctk.CTkButton(self,width=200,height=40,fg_color="blue",text="確認查詢",font=("Arial",20))
+        confirm_btn = ctk.CTkButton(self,width=200,height=40,fg_color="blue",text="確認查詢",font=("Arial",20),command=search)
         reset_btn = ctk.CTkButton(self,width=200,height=40,fg_color="green",text="重設查詢",font=("Arial",20),command=reset)
         right_top = right_top_part(self,width=1200,height=200,fg_color="#EEEEEE")
         right_bot = right_bot_part(self,width=1200,height=500,fg_color="#EEEEEE")
@@ -48,7 +92,7 @@ class left_part(ctk.CTkFrame):
         right_top.place(x=270,y=0)
         right_bot.place(x=270,y=220)
 
-class right_bot_part(ctk.CTkFrame):
+class top_bar(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         bar_1 = ctk.CTkLabel(self, width=176, height=40,text="選擇")
@@ -64,11 +108,24 @@ class right_bot_part(ctk.CTkFrame):
         bar_4.place(x=528,y=0)
         bar_5.place(x=704,y=0)
         bar_6.place(x=880,y=0)
+class right_bot_part(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        top = top_bar(self,width=1200,height=40)
+        self.mid = ctk.CTkScrollableFrame(self,width=1150,height=420,fg_color="#EEEEEE")
+        bot = ctk.CTkFrame(self,width=1200,height=40)
+        self.label = ctk.CTkLabel(bot,width=50,height=40,text="總計：",font=("Arial",20))
+        self.btn = ctk.CTkButton(bot,text="入賬")
+        top.place(x=0,y=0)
+        self.mid.place(x=0,y=40)
+        bot.place(x=0,y=440)
+        self.label.place(x=0,y=0)
+        self.btn.place(x=1030,y=5)
 class Accounting_Main_Frame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        self.left = left_part(self, width=1450,height=750,fg_color="#EEEEEE")
+        self.left = left_part(self, width=1450,height=700,fg_color="white")
         # self.right_top = right_top_part(self,width=1200,height=200,fg_color="#EEEEEE")
         # self.right_bot = right_bot_part(self,width=1200,height=500,fg_color="#EEEEEE")
 
--- Order/order.py
+++ Order/order.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 import tkcalendar as tkc
+import psycopg2
+
 class bar(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
@@ -23,25 +25,118 @@ class bar(ctk.CTkFrame):
         self.bar_8.place(x=1420,y=10)
         self.bar_9.place(x=1620,y=10)
 
+class entrybox(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.serial = ctk.CTkEntry(self,width=200,height=40)
+        self.item_id = ctk.CTkEntry(self,width=200,height=40)
+        self.item_name = ctk.CTkEntry(self,width=200,height=40)
+        self.specification = ctk.CTkEntry(self,width=200,height=40)
+        self.size = ctk.CTkEntry(self,width=200,height=40)
+        self.price = ctk.CTkEntry(self,width=200,height=40)
+        self.quantity = ctk.CTkEntry(self,width=200,height=40)
+        self.subtotal = ctk.CTkEntry(self,width=200,height=40)
+        self.remark = ctk.CTkEntry(self,width=200,height=40)
+
+        self.serial.grid(row=0,column=0)
+        self.item_id.grid(row=0,column=1)
+        self.item_name.grid(row=0,column=2)
+        self.specification.grid(row=0,column=3)
+        self.size.grid(row=0,column=4)
+        self.price.grid(row=0,column=5)
+        self.quantity.grid(row=0,column=6)
+        self.subtotal.grid(row=0,column=7)
+        self.remark.grid(row=0,column=8)
+
 class top(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.c_id = ctk.CTkEntry(self,width=160,height=40,placeholder_text="輸入客戶代號",font=("Arial",24))
-        self.cal = tkc.DateEntry(self,)
+        self.c_id = ctk.CTkEntry(self,width=250,height=50,placeholder_text="輸入客戶代號",font=("Arial",24))
+        self.cal = tkc.DateEntry(self)
         self.c_id.place(x=40,y=30)
-        self.cal.place(x=220,y=30)
-        self.bot = bot(self,width=1800,height=630,fg_color="#EEEEEE")
+        self.cal.place(x=310,y=30)
+        self.bot = bot(self,width=1800,height=630,border_width=3,border_color="green",c_id=self.c_id,cal=self.cal)
         self.bot.place(x=0,y=120)
 
 class bot(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, c_id, cal, **kwargs):
         super().__init__(master, **kwargs)
-        self.top_bar = bar(self,width=1800,height=40)
-        self.top_bar.place(x=0,y=40)
+        def next_row(event):
+            self.entry_1 = entrybox(self.mid_frame)
+            self.entry_1.pack()
+            self.entry_1.serial.focus()
+            self.entry_1.remark.bind('<Return>',temp_data)
+            self.entry_1.remark.bind('<Return>',next_row)
+            self.entry_1.item_id.bind('<Return>',item_name)
+            self.entry_1.quantity.bind('<Return>',total_price)
+        def item_name(event):
+            con = psycopg2.connect(database='postgres', user='postgres',
+                       password='admin')
 
+            cur = con.cursor()
+            cur.execute(f"SELECT item_name from item where item_id = '{self.entry_1.item_id.get()}'")
+            result = cur.fetchone()
+            cur.close()
+            self.entry_1.item_name.insert(0,str(result[0]).rstrip())
+            self.entry_1.specification.focus()
+            
+        def total_price(event):
+            price = self.entry_1.price.get()
+            quan = self.entry_1.quantity.get()
+            total = int(price) * int(quan)
+            self.totalsum += total
+            self.entry_1.subtotal.insert(0,total)
+            self.entry_1.remark.focus()
+            self.total.configure(text="總計："+str(self.totalsum))
+        def temp_data(event):
+            self.temp = []
+            self.temp.append(self.entry_1.item_id.get())
+            self.temp.append(self.entry_1.item_name.get())
+            self.temp.append(self.entry_1.specification.get())
+            self.temp.append(self.entry_1.size.get())
+            self.temp.append(self.entry_1.price.get())
+            self.temp.append(self.entry_1.quantity.get())
+            self.temp.append(self.entry_1.subtotal.get())
+            self.temp.append(self.entry_1.remark.get())
+            self.temp.append(c_id.get())
+            self.temp.append(cal.get_date())
+            self.save_file.append(self.temp)
+        def save_data():
+            con = psycopg2.connect(database='postgres', user='postgres',
+                       password='admin')
+            cur = con.cursor()
+            for i in self.save_file:
+                cur.execute(f"insert into order_it(item_id, item_name, specification, size, price, quantity, subtotal, remark, c_id, date)\
+                            values('{i[0]}','{i[1]}','{i[2]}','{i[3]}','{i[4]}','{i[5]}','{i[6]}','{i[7]}','{i[8]}','{i[9]}')")
+            cur.close()
+            con.commit()
+            con.close()
+            # print(self.save_file)
+        self.top_bar = bar(self,width=1800,height=40,fg_color="#EEEEEE")
+        self.top_bar.place(x=0,y=0)
+        self.mid_frame = ctk.CTkScrollableFrame(self,width=1780,height=550)
+        self.mid_frame.place(x=0,y=40)
+        self.bot_frame = ctk.CTkFrame(self,width=1800,height=40,fg_color="#EEEEEE")
+        self.bot_frame.place(x=0,y=590)
+        self.btn_exit = ctk.CTkButton(self.bot_frame,width=120,height=20,text="離開")
+        self.btn_save = ctk.CTkButton(self.bot_frame,width=120,height=20,text="存檔",command=save_data)
+        self.btn_exit.place(x=1510,y=10)
+        self.btn_save.place(x=1650,y=10)
+        self.total = ctk.CTkLabel(self.bot_frame,text="總計：",font=("Arial",20))
+        self.total.place(x=0,y=7)
+        self.totalsum = 0
+        self.save_file = []
+        self.entry_1 = entrybox(self.mid_frame)
+        self.entry_1.pack()
+        self.entry_1.item_id.bind('<Return>',item_name)
+        self.entry_1.remark.bind('<Return>',temp_data)
+        self.entry_1.remark.bind('<Return>',next_row)
+        self.entry_1.quantity.bind('<Return>',total_price)
+        
 class Order_Main_Frame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
         top_part = top(self,width=1800,height=750)
         top_part.place(x=60,y=60)
+        
